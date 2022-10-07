4c6d75420954739232d3cea0bda9561411aa1b63
abdwFawzy <abdwfawzy985@gmail.com>
2022-10-07 06:42:33 +0200

    Pagination

--- API/settings.py
+++ API/settings.py
@@ -140,5 +140,7 @@ REST_FRAMEWORK = {
     'DEFAULT_PERMISSION_CLASSES': [
         'rest_framework.permissions.IsAuthenticated'
     ],
-    'DEFAULT_FILTER_BACKENDS': ['url_filter.integrations.drf.DjangoFilterBackend']
+    'DEFAULT_FILTER_BACKENDS': ['url_filter.integrations.drf.DjangoFilterBackend'],
+    'DEFAULT_PAGINATION_CLASS': 'drf_link_header_pagination.LinkHeaderPagination',
+    'PAGE_SIZE': 1,
 }
