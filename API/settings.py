7102615dd3f906eab64238295548b86ebb0e1492
abdwFawzy <abdwfawzy985@gmail.com>
2022-10-10 02:32:32 +0200

    Fix a bug

--- API/settings.py
+++ API/settings.py
@@ -108,7 +108,7 @@ AUTH_PASSWORD_VALIDATORS = [
 ]
 
 
-# Internationalization
+# Internatiostringnalization
 # https://docs.djangoproject.com/en/3.2/topics/i18n/
 
 LANGUAGE_CODE = 'en-us'
@@ -138,6 +138,7 @@ REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': [
         'rest_framework.authentication.SessionAuthentication',
         'rest_framework_simplejwt.authentication.JWTAuthentication',
+
 
     ],
     'DEFAULT_PERMISSION_CLASSES': [
--- API/urls.py
+++ API/urls.py
@@ -44,5 +44,6 @@ urlpatterns = [
     re_path(r'^redoc/$', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     path('api/token/verify/', TokenVerifyView.as_view(), name='token_verify'), 
-    path('api/v2/', include('API.routers'))
+    path('api/v2/', include('API.routers')),
+    path('admin/', admin.site.urls),
 ]
